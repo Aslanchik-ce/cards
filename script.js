6a594586cedd78a996e66fd4c86764d03c0e8859
Aslanchik-ce <aslanmoldash3@gmail.com>
2024-11-19 11:11:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.more-info').forEach(button => {
+    button.addEventListener('click', function () {
+      const card = this.closest('.card');
+      card.classList.toggle('open');
+    });
+  });
